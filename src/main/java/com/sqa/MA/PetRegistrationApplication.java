2d43ff165be95e2c987167e7ab2b3d312321f6ef
mikias ayalew <ammrdf@yahoo.com>
2016-04-02 10:42:06 -0700

    PetRegistrationApplication

---
+++ src/main/java/com/sqa/MA/PetRegistrationApplication.java
@@ -0,0 +1,35 @@
+/**
+ *   File Name: PetRegistrationApplication.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Apr 2, 2016
+ *   
+ */
+
+package com.sqa.MA;
+
+/**
+ * PetRegistrationApplication //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      LastName, FirstName
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class PetRegistrationApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
